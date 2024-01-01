808a10aae8cf9c21ce0457f438d7335bf8ac6712
Javalover93 <javalover93@163.com>
2024-01-01 21:26:00 +0800

    Add basic features

--- main.ts
+++ main.ts
@@ -1,73 +1,109 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+const cheerio = require('cheerio');
+const https = require('https');
 
-// Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface PasteURLPluginSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: PasteURLPluginSettings = {
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class PasteURLPlugin extends Plugin {
+	settings: PasteURLPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
+		console.log('我进来了');
+
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		const ribbonIconEl = this.addRibbonIcon('cherry', 'Paste URL', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+			new PasteURLModal(this.app, (result) => {
+                let title = '';
+                let paste_url = '';
+                
+                https.get(result, (res) => {
+                
+                    let data = ''
+                    res.on('data', (d) => {
+                    data += d;
+                    });
+                
+                    res.on('end', () => {
+                    const $ = cheerio.load(data)
+                    title = $('title').text();
+                    console.log(`title is ${title}`);
+                    paste_url = `[${title}](${result})`;
+                    console.log(`[${title}](${result})`);
+
+                    // new Notice(`${paste_url}的标题是${title}`);
+            
+                    const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+                    // Make sure the user is editing a Markdown file.
+                    if (view) {
+                        const cursor = view.editor.getCursor();
+                        view.editor.replaceRange(paste_url, cursor);
+                    }
+                    });
+                
+                }).on('error', (e) => {
+                    console.error(e);
+                    paste_url = 'Error';
+                }); 
+
+            }).open();
 		});
+        
 		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		ribbonIconEl.addClass('paste-url-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+		// This adds a simple command that can be triggered anywhere for pasteurl
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'past-url',
+			name: 'Paste URL',
 			callback: () => {
-				new SampleModal(this.app).open();
+				new PasteURLModal(this.app, (result) => {
+                    let title = '';
+                    let paste_url = '';
+                    
+                    https.get(result, (res) => {
+                    
+                        let data = ''
+                        res.on('data', (d) => {
+                        data += d;
+                        });
+                    
+                        res.on('end', () => {
+                        const $ = cheerio.load(data)
+                        title = $('title').text();
+                        console.log(`title is ${title}`);
+                        paste_url = `[${title}](${result})`;
+                        console.log(`[${title}](${result})`);
+
+                        // new Notice(`${paste_url}的标题是${title}`);
+                
+                        const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+                        // Make sure the user is editing a Markdown file.
+                        if (view) {
+                            const cursor = view.editor.getCursor();
+                            view.editor.replaceRange(paste_url, cursor);
+                        }
+                        });
+                    
+                    }).on('error', (e) => {
+                        console.error(e);
+                        paste_url = 'Error';
+                    }); 
+
+                }).open();
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
+		
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
 		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
@@ -79,6 +115,7 @@ export default class MyPlugin extends Plugin {
 	}
 
 	onunload() {
+		console.log('我又出去了');
 
 	}
 
@@ -91,44 +128,50 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+
+class PasteURLModal extends Modal {
+	result: string;
+
+	onSubmit: (result: string) => void
+
+	constructor(app: App, onSubmit: (result: string) => void) {
 		super(app);
+		this.onSubmit = onSubmit;
 	}
 
 	onOpen() {
 		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		contentEl.createEl("h1", { text: "请输入URL："});
+
+		new Setting(contentEl)
+		.setName("URL")
+		.addText((text) =>
+		  text.onChange((value) => {
+			this.result = value
+		  }));
+
+        new Setting(contentEl)
+		.addButton((btn) =>
+			btn
+			.setButtonText("提交")
+			.setCta()
+			.onClick(() => {
+				this.close();
+				this.onSubmit(this.result);
+                // console.log(this.result);
+			}));
+        
+        contentEl.addEventListener("keyup", ({key}) => {
+            if( key == 'Enter' && this.result !== undefined) {
+                this.close();
+				this.onSubmit(this.result);
+            }
+        });
+		
 	}
 
 	onClose() {
 		const {contentEl} = this;
 		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
